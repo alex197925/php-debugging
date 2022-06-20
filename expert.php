5ba3fe323f42cc9ed6721b7d260929d0dc3efafc
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-06-20 15:53:10 +0200

    exercise 9 done

--- expert.php
+++ expert.php
@@ -133,6 +133,26 @@ echo login('wrong@example.be', 'wrong');
 
 
 
+new_exercise(9);
+function isLinkValid(string $link) {
+    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+
+    foreach ($unacceptables as $unacceptable) {
+        if (strpos($link, $unacceptable) !== false) {
+            return 'Unacceptable Found<br />';
+        }
+    }
+    return 'Acceptable<br />';
+}
+//invalid link
+echo isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+echo isLinkValid('https://google.com');
+//VALID link
+echo isLinkValid('http://google.com');
+//VALID link
+echo isLinkValid('http://google.com/test.txt');
+
 
 
 
