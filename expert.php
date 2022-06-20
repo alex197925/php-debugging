6578d591a228f153a74815aae69b189ed9300b91
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-06-20 11:06:17 +0200

    exercise 4,5 done

--- expert.php
+++ expert.php
@@ -34,6 +34,7 @@ $str = 'Debugged ! Also very fun';
 echo substr($str, 0, 10);
 
 new_exercise(4);
+
 // === Exercise 4 ===
 // Sometimes debugging code is just like looking up code and syntax...
 // The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
@@ -44,3 +45,23 @@ foreach($week as $day) {
 }
 
 print_r($week);
+
+
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alfabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+
+//$arr = [];
+//for ($letter = 'a'; $letter <= 'z'; $letter++) {
+//    array_push($arr, $letter);
+//}
+//
+//print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
+$arr = [];
+foreach(range('a','z') as $letter)
+{
+    //echo "$letter<br/>";
+    array_push($arr, $letter);
+}
+print_r($arr);
